a22b3aa500bfd2a5fb257b6c3a7f5cf2641b0d72
Danny Bronshtein <danibr1994@gmail.com>
2025-06-03 16:41:17 +0300

    Refactor main.rs: update file handling to use TOML format and improve topic generation logic

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::env;
 use daily_notes_reminder::common::email::Email;
 
 use daily_notes_reminder::common::topics_generator::TopicsGenerator;
-use daily_notes_reminder::common::topics_generator::Topics;
-use daily_notes_reminder::common::topics_generator_factory::topics_gen_factory;
+use daily_notes_reminder::common::topics_generator::BlogPosts;
+use daily_notes_reminder::tomlfile_topics_gen::TomlFileTopicsGenerator;
+//use daily_notes_reminder::common::topics_generator_factory::topics_gen_factory;
 
 use daily_notes_reminder::common::email_factory::email_factory;
 
@@ -16,12 +17,18 @@ use daily_notes_reminder::s3_manager::S3Manager;
 use lambda_runtime::{service_fn, LambdaEvent, Error};
 use serde_json::Value;
 
-const FILE: &str = "coding_notes.txt";
-const FILE_PATH: &str = "/tmp/coding_notes.txt";
+const FILE: &str = "coding_blog.toml";
+const FILE_PATH: &str = "/tmp/coding_blog.toml";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     lambda_runtime::run(service_fn(lambda_handler)).await
+    // let _ = match run().await {
+    //     Ok(_) => println!("Execution completed"),
+    //     Err(e) => eprintln!("Error: {}", e),
+    // };
+    
+    //Ok(())
    }
 
 async fn lambda_handler(event: LambdaEvent<Value>) -> Result<String, Error> {
@@ -39,24 +46,26 @@ async fn run() -> Result<(), Box<dyn std::error::Error>> {
         Err(e) => eprintln!("Error: {}", e),
     };
 
-    let topics = match topics_gen_factory(FILE_PATH) {
-        Some(factory) => factory.generate_topics(FILE_PATH)?,
-        None => return Err("Unsupported file type".into()),
+    let toml_topics_gen = TomlFileTopicsGenerator {};
+    let posts = match toml_topics_gen.generate_topics(FILE_PATH) {
+        Ok(topics) => topics,
+        Err(e) => return Err(e),
+        
     };
 
     let email_handler = email_factory();
     let email_content = subject_gen_factory();
 
-    let _ = send_email(&topics, &email_handler, &email_content)?;
+    let _ = send_email(&posts, &email_handler, &email_content)?;
 
     Ok(())
 }
 
-fn send_email(topics: &Topics, email_handler: &impl Email, subject_generator: &impl SubjectGenerator) -> Result<(), Box<dyn std::error::Error>> {
+fn send_email(posts: &BlogPosts, email_handler: &impl Email, subject_generator: &impl SubjectGenerator) -> Result<(), Box<dyn std::error::Error>> {
     
     let subject:    &str = "Daily Code Principles";
     
-    let email_content: String = subject_generator.generate_subject(&topics)?;
+    let email_content: String = subject_generator.generate_subject(&posts)?;
 
     email_handler.send_email(subject, email_content)?;
 
